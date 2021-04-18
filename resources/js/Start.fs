1334b73d90f756a7e34eb714c6f195dfa0a07806
Mario Bašić <mario@laravelista.hr>
2021-04-18 12:39:30 +0200

    Removes comments from Start.fs.

--- resources/js/Start.fs
+++ resources/js/Start.fs
@@ -1,22 +1,8 @@
 module Start
 
-// Feliz
-
 open Feliz
 open Browser.Dom
+
 open GetStarted
 
-// [<ReactComponent(import="default", from="./components/GetStarted.js")>]
-// let GetStarted () = React.imported()
-
 ReactDOM.render(GetStarted(), document.getElementById "get-started")
-
-// Fable-react
-
-// open Fable.React
-// open Browser.Dom
-
-// let inline get_started () : ReactElement =
-//     ofImport "default" "./components/GetStarted.js" () []
-
-// ReactDom.render(get_started (), document.getElementById "get-started")
