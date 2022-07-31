63381fcb474e6d944db18f04ecba281586cffcac
Mario Bašić <mario@laravelista.hr>
2022-07-31 10:18:03 +0200

    Improves fast/slow loading. Updates backers page.

--- public/index.php
+++ public/index.php
@@ -2,11 +2,19 @@
 
 declare(strict_types=1);
 
+$phelMode = getenv('PHEL_MODE');
+
+file_put_contents("php://stdout", "\nPhel mode: $phelMode\n");
+
 $projectRootDir  = __DIR__ . '/../';
 
-// Compiles Phel code to PHP on each request and then runs it (Slow).
-// require $projectRootDir . 'vendor/autoload.php';
-// Phel\Phel::run($projectRootDir, 'app\\boot');
+if ($phelMode === 'slow') {
+    // Compiles Phel code to PHP on each request and then runs it (Slow).
+    require $projectRootDir . 'vendor/autoload.php';
+    Phel\Phel::run($projectRootDir, 'app\\boot');
+
+    return;
+}
 
 // Runs previously compiled PHP code (Fast).
 require_once $projectRootDir . "vendor/autoload.php";
