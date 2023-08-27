3e1e7212d1c9d413bfa0306a53886865f7dffad1
Mario Bašić <mario@laravelista.hr>
2023-08-27 15:11:16 +0200

    feat: attempt n1 to upgrade to latest phel

--- phel-config.php
+++ phel-config.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
-return [
-    'src-dirs' => ['src'],
-    'test-dirs' => ['tests'],
-    'out-dir' => 'dist'
-];
+use Phel\Config\PhelConfig;
+use Phel\Config\PhelOutConfig;
+
+return (new PhelConfig())
+    ->setSrcDirs(['src'])
+    ->setTestDirs(['tests'])
+    ->setOut(
+        (new PhelOutConfig())
+            ->setDestDir('out')
+            ->setMainPhelNamespace('cli-skeleton\main')
+            ->setMainPhpFilename('index')
+    )
+    ->setFormatDirs(['src', 'tests'])
+    ->setIgnoreWhenBuilding(['local.phel'])
+    ->setKeepGeneratedTempFiles(false);
